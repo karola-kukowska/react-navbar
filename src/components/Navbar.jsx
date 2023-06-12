5e89f044e53586859f2dea08de0fcfc563cc96e1
karolina.kukowska@gmail.com <64924406+karola-kasprzak@users.noreply.github.com>
2023-06-12 15:50:16 +0200

    links dynamic functionality

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import { useState, useRef } from "react";
+import { FaBars } from "react-icons/fa";
+import { links, social } from "../data";
+import logo from "../assets/logo.svg";
+
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  const linkStyles = {
+    height: showLinks
+      ? `${linksRef.current.getBoundingClientRect().height}%`
+      : "0px",
+  };
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="Codding Addict" className="logo" />
+          <button
+            type="button"
+            className="nav-toggle"
+            onClick={() => setShowLinks(!showLinks)}
+          >
+            <FaBars />
+          </button>
+        </div>
+      </div>
+
+      <div
+        className="links-container"
+        ref={linksContainerRef}
+        style={linkStyles}
+      >
+        <ul className="links" ref={linksRef}>
+          {links.map((link) => (
+            <li key={link.id}>
+              <a href={link.url}>{link.text}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
